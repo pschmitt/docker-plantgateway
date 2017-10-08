6d895f3cfa50cfd34aea40c3945be43dab7c64fb
Philipp Schmitt <philipp@schmitt.co>
2017-10-08 20:56:01 +0200

    Run non-interactively

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd "$(readlink -f "$(dirname "$0")")" || exit 9
 
-docker run -it --rm \
+docker run --rm \
   --cap-add NET_ADMIN --net=host --device /dev/ttyAMA0 \
   -v ${PWD}/config:/config:ro \
   pschmitt/plantgateway
