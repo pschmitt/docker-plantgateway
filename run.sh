221572a76f2c7e4f6bb3253087a3bc2ff51fc523
Philipp Schmitt <philipp@schmitt.co>
2017-10-08 12:30:26 +0200

    Allow container to init bt device

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 cd "$(readlink -f "$(dirname "$0")")" || exit 9
 
 docker run -it --rm \
-  --net=host \
+  --cap-add NET_ADMIN --net=host --device /dev/ttyAMA0 \
   -v ${PWD}/config:/config:ro \
   pschmitt/plantgateway
